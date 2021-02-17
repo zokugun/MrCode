bfcdfaeefa97beffb563d707efac3a776f89a22a
Baptiste Augrain <daiyam@zokugun.org>
2021-02-18 00:22:55 +0100

    update patches

--- build_macos_dev.sh
+++ build_macos_dev.sh
@@ -4,6 +4,6 @@ set -o allexport; source .env; set +o allexport
 
 cd vscodium
 
-gsed -i -E 's/\.\/prepare\_vscode\.sh/# .\/prepare_vscode.sh/g' build.sh
+gsed -i -E 's/\. prepare\_vscode\.sh/# . prepare_vscode.sh/g' build.sh
 
-SHOULD_BUILD=yes CI_BUILD=no OS_NAME=osx VSCODE_ARCH=x64 LATEST_MS_COMMIT='1.52.1'. build.sh
+SHOULD_BUILD=yes CI_BUILD=no OS_NAME=osx VSCODE_ARCH=x64 LATEST_MS_COMMIT='1.53.2'. build.sh
