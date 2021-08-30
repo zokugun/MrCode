3fff6d6aa989b3429f93c08f9aa484a06293f9e3
Baptiste Augrain <daiyam@zokugun.org>
2021-08-30 16:43:30 +0200

    ci(aur): fix mrcode-git

--- stores/aur/update_mrcode_git.sh
+++ stores/aur/update_mrcode_git.sh
@@ -2,15 +2,12 @@
 
 set -o errexit -o pipefail -o nounset
 
-pwd
-ls -la
-
 new_version=$( git describe --long --tags | sed 's/\([^-]*-g\)/r\1/;s/-/./g' )
 echo "new_version: $new_version"
 
 cd stores/aur/mrcode-git
 
-old_version=$( cat "PKGBUILD" | sed -n "s/.*pkgver=\([0-9.+]*\).*/\1/p" )
+old_version=$( cat "PKGBUILD" | sed -n "s/.*pkgver=\([a-z0-9.+]*\).*/\1/p" )
 echo "old_version: $old_version"
 
 if [[ "$new_version" != "$old_version" ]]; then
@@ -18,8 +15,11 @@ if [[ "$new_version" != "$old_version" ]]; then
     sed -i "s/pkgrel=.*$/pkgrel=1/" PKGBUILD
 
     changes=$( git status > /dev/null 2>&1 && git diff-index --quiet HEAD && echo 'no' || echo 'yes' )
+    echo "changes: $changes"
 
     if [[ "$changes" == "yes" ]]; then
+        echo "Update mrcode to ${new_version}"
+
         git commit -m "Update mrcode to ${new_version}"
         git push
     else
