a6a8590b5d5bcd239968e0f72a320a062f6f354a
Baptiste Augrain <daiyam@zokugun.org>
2021-08-30 13:25:23 +0200

    ci(aur): test ssh

--- stores/aur/setup.sh
+++ stores/aur/setup.sh
@@ -6,13 +6,20 @@ echo "Setting up ssh"
 
 mkdir -p  ~/.ssh
 
-ssh-keyscan aur.archlinux.org >> ~/.ssh/known_hosts
+ssh-keyscan -v -t ed25519 aur.archlinux.org >> ~/.ssh/known_hosts
 
 cp ssh_config ~/.ssh
 
 echo -e "${SSH_PRIVATE_KEY//_/\\n}" > ~/.ssh/aur
 
-chmod 600 ~/.ssh/aur*
+chmod -vR 600 ~/.ssh/aur*
+
+ssh-keygen -vy -f ~/.ssh/aur > ~/.ssh/aur.pub
+
+sha512sum ~/.ssh/aur ~/.ssh/aur.pub
+
+echo "Test ssh"
+ssh -Tv aur@aur.archlinux.org
 
 echo "Setting up git"
 git config --global user.name "$GIT_USERNAME"
