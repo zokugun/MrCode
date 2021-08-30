8dbf6748ec2810be84a03327a1cd54d1c5a5e25c
Baptiste Augrain <daiyam@zokugun.org>
2021-08-30 15:54:26 +0200

    ci(aur): publish mrcode-bin and mrcode-git

---
+++ stores/aur/update_mrcode_bin.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -o errexit -o pipefail -o nounset
+
+cd mrcode-bin
+
+new_version=$( curl --silent "https://api.github.com/repos/zokugun/MrCode/releases/latest" | jq -r .tag_name )
+echo "new_version: $new_version"
+
+old_version=$( cat "PKGBUILD" | sed -n "s/.*pkgver=\([0-9.+]*\).*/\1/p" )
+echo "old_version: $old_version"
+
+if [[ "$new_version" != "$old_version" ]]; then
+    sed -i "s/pkgver=.*$/pkgver=${new_version}/" PKGBUILD
+    sed -i "s/pkgrel=.*$/pkgrel=1/" PKGBUILD
+
+    updpkgsums
+
+    changes=$( git status > /dev/null 2>&1 && git diff-index --quiet HEAD && echo 'no' || echo 'yes' )
+
+    if [[ "$changes" == "yes" ]]; then
+        git commit -m "Update mrcode to ${new_version}"
+        git push
+    else
+        echo "No changes"
+    fi
+else
+    echo "Already published"
+fi
