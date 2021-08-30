8a34d3e1049af8358d8a8ee52573b0ece8891be8
Baptiste Augrain <daiyam@zokugun.org>
2021-08-30 15:41:36 +0200

    ci(aur): fix update script

--- stores/aur/update_mrcode.sh
+++ stores/aur/update_mrcode.sh
@@ -4,20 +4,20 @@ set -o errexit -o pipefail -o nounset
 
 cd mrcode
 
-git_version=$( curl --silent "https://api.github.com/repos/zokugun/MrCode/releases/latest" | jq -r .tag_name )
-echo "git_version: $git_version"
+new_version=$( curl --silent "https://api.github.com/repos/zokugun/MrCode/releases/latest" | jq -r .tag_name )
+echo "new_version: $new_version"
 
-aur_version=$( cat "PKGBUILD" | sed -n "s/.*pkgver=\([0-9.+]*\).*/\1/p" )
-echo "aur_version: $aur_version"
+old_version=$( cat "PKGBUILD" | sed -n "s/.*pkgver=\([0-9.+]*\).*/\1/p" )
+echo "old_version: $old_version"
 
-if [[ "$git_version" == "$aur_version" ]]; then
-    sed -i "s/pkgver=.*$/pkgver=${git_version}/" PKGBUILD
+if [[ "$new_version" != "$old_version" ]]; then
+    sed -i "s/pkgver=.*$/pkgver=${new_version}/" PKGBUILD
     sed -i "s/pkgrel=.*$/pkgrel=1/" PKGBUILD
 
-    changes=$( git diff-index --quiet HEAD && echo 'no' || echo 'yes' )
+    changes=$( git status > /dev/null 2>&1 && git diff-index --quiet HEAD && echo 'no' || echo 'yes' )
 
     if [[ "$changes" == "yes" ]]; then
-        git commit -m "Update mrcode to ${RELEASE_VERSION}"
+        git commit -m "Update mrcode to ${new_version}"
         git push
     else
         echo "No changes"
