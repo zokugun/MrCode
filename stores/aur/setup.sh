f34be5d944c911decf386a99850d4c05f708d2dc
Baptiste Augrain <daiyam@zokugun.org>
2021-08-30 13:10:58 +0200

    ci(aur): setup ssh and git

---
+++ stores/aur/setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -o errexit -o pipefail -o nounset
+
+echo "Setting up ssh"
+ssh-keyscan aur.archlinux.org >> ~/.ssh/known_hosts
+
+cp ssh_config ~/.ssh
+
+echo -e "${SSH_PRIVATE_KEY//_/\\n}" > ~/.ssh/aur
+
+chmod 600 ~/.ssh/aur*
+
+echo "Setting up git"
+git config --global user.name "$GIT_USERNAME"
+git config --global user.email "$GIT_EMAIL"
