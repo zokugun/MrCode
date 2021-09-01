328d7993d0cec02c219f8c6be139508a4767dc2c
Baptiste Augrain <daiyam@zokugun.org>
2021-09-01 16:37:07 +0200

    ci(aur): fix action

--- .github/actions/publish-aur-package/entrypoint.sh
+++ .github/actions/publish-aur-package/entrypoint.sh
@@ -88,7 +88,7 @@ if [[ "$changes" == "yes" ]]; then
 
     if [[ "${INPUT_SKIP_TEST}" != "yes" ]]; then
         echo "Testing package"
-        su builder -c "makepkg --noconfirm -s -c"
+        su builder -c "GITHUB_ENV='' makepkg --noconfirm -s -c"
     else
         echo "Skipping testing"
     fi
