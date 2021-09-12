0937a5587cd36547841089c016cf7466a1531562
Baptiste Augrain <daiyam@zokugun.org>
2021-09-12 02:46:14 +0200

    fix: fixing linux workflow

--- version.sh
+++ version.sh
@@ -4,15 +4,21 @@ set -e
 
 if [[ -z "${BUILD_SOURCEVERSION}" ]]; then
 
+    pwd
+    ls -la
+
     npm install -g checksum
 
     mrcode_hash=$( git rev-parse HEAD )
+    echo "mrcode_hash: $mrcode_hash"
 
     cd vscodium
     vscodium_hash=$( git rev-parse HEAD )
+    echo "vscodium_hash: $vscodium_hash"
 
     cd vscode
     vscode_hash=$( git rev-parse HEAD )
+    echo "vscode_hash: $vscode_hash"
 
     cd ../..
 
