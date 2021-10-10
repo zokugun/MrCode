0db28d39c0d1e3c4a7f14826c8363290c2ab987a
Baptiste Augrain <daiyam@zokugun.org>
2021-10-10 11:04:59 +0200

    fix: update filenames of msi

--- prepare.sh
+++ prepare.sh
@@ -77,6 +77,8 @@ gsed -i -E 's/yarn --frozen-lockfile/yarn/g' prepare_vscode.sh
 backup 'prepare_artifacts.sh'
 gsed -i -E 's/VSCodium/MrCode/g' prepare_artifacts.sh
 gsed -i -E 's/MS_TAG/RELEASE_VERSION/g' prepare_artifacts.sh
+gsed -i 's|MrCode-${VSCODE_ARCH}-${RELEASE_VERSION}.msi artifacts/|MrCode-${VSCODE_ARCH}-${MS_TAG}.msi artifacts\\\\MrCode-${VSCODE_ARCH}-${RELEASE_VERSION}.msi|' prepare_artifacts.sh
+gsed -i 's|MrCode-${VSCODE_ARCH}-updates-disabled-${RELEASE_VERSION}.msi artifacts/|MrCode-${VSCODE_ARCH}-updates-disabled-${MS_TAG}.msi artifacts\\\\MrCode-${VSCODE_ARCH}-updates-disabled-${RELEASE_VERSION}.msi|' prepare_artifacts.sh
 
 # check_tags.sh
 backup 'check_tags.sh'
@@ -96,7 +98,6 @@ gsed -i -E 's/ASSET_NAME=MrCode-darwin-\$\{MS_TAG\}\.zip/ASSET_NAME=MrCode-darwi
 gsed -i -E 's|VERSION_PATH="darwin"|VERSION_PATH="darwin/${VSCODE_ARCH}"|g' update_version.sh
 gsed -i -E 's/MS_TAG/RELEASE_VERSION/g' update_version.sh
 gsed -i -E 's/MS_COMMIT/BUILD_SOURCEVERSION/g' update_version.sh
-gsed -i -E 's/set -e/set -ex/' update_version.sh
 
 # release.sh
 backup 'release.sh'
