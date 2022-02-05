7f1462068f8ce901897f3ed15d20139bb08e5b88
Baptiste Augrain <daiyam@zokugun.org>
2022-02-05 16:02:21 +0100

    fix(1.64): update patches

--- build/build_linux_dev.sh
+++ build/build_linux_dev.sh
@@ -11,6 +11,6 @@ fi
 
 . version.sh
 
-gsed -i -E 's/\. prepare\_vscode\.sh/# . prepare_vscode.sh/g' build.sh
+sed -i -E 's/\. prepare\_vscode\.sh/# . prepare_vscode.sh/g' build.sh
 
-SHOULD_BUILD=yes CI_BUILD=no OS_NAME=linux VSCODE_ARCH=x64 RELEASE_VERSION='1.58.2' . build.sh
+SHOULD_BUILD=yes CI_BUILD=no OS_NAME=linux VSCODE_ARCH=x64 RELEASE_VERSION='1.64.0' . build.sh
