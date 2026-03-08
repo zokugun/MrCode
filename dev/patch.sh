acc762dea47a5061b280711634d0cf04f79e88dc
Baptiste Augrain <daiyam@zokugun.org>
2026-03-08 01:54:36 +0100

    feat: use latest vscodium

--- dev/patch.sh
+++ dev/patch.sh
@@ -2,7 +2,19 @@
 
 set -e
 
-echo "$#"
+normalize_file() {
+  if [[ "${1}" == *patch ]]; then
+    FILE="${1}"
+  else
+    FILE="${1}.patch"
+  fi
+
+  if [[ "${FILE}" == patches/* ]]; then
+    FILE="../../${FILE}"
+  else
+    FILE="../../patches/${FILE}"
+  fi
+}
 
 cd vscodium || { echo "'vscodium' dir not found"; exit 1; }
 cd vscode || { echo "'vscode' dir not found"; exit 1; }
@@ -16,15 +28,17 @@ done
 
 git apply --reject "../patches/helper/settings.patch"
 
+for FILE in ../patches/*.patch; do
+  if [[ -f "${FILE}" ]]; then
+    git apply --reject "${FILE}" || true
+  fi
+done
+
 while [ $# -gt 1 ]; do
   echo "Parameter: $1"
-  if [[ "${1}" == *patch ]]; then
-    FILE="../../patches/${1}"
-  else
-    FILE="../../patches/${1}.patch"
-  fi
+  normalize_file "${1}"
 
-  git apply --reject "${FILE}"
+  git apply --reject "${FILE}" || true
 
   shift
 done
@@ -32,17 +46,20 @@ done
 git add .
 git commit --no-verify -q -m "VSCODIUM HELPER"
 
-if [[ "${1}" == *patch ]]; then
-  FILE="../../patches/${1}"
-else
-  FILE="../../patches/${1}.patch"
-fi
+normalize_file "${1}"
+
+echo "${FILE}"
 
 if [[ -f "${FILE}" ]]; then
   git apply --reject "${FILE}" || true
 fi
 
 read -rp "Press any key when the conflict have been resolved..." -n1 -s
+
+# while [[ -n "$( find . -name '*.rej' -print )" ]]; do
+#   echo
+#   read -rp "Press any key when the conflict have been resolved..." -n1 -s
+# done
 
 git add .
 git diff --staged -U1 > "${FILE}"
